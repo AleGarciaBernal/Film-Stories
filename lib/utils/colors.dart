579629c5f3d9c20e7bfe6d6da0227743ca4f96b2
aleandy7 <80707995+AleGarciaBernal@users.noreply.github.com>
2022-12-21 01:02:29 -0400

    Fix: Change Colors Palette

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
-const mobileBackgroundColor = Color.fromARGB(255, 38, 81, 156);
-const webBackgroundColor = Color.fromARGB(255, 38, 81, 156);
-//const mobileSearchColor = Color.fromRGBO(38, 38, 38, 1);
+const mobileBackgroundColor = Color.fromARGB(255, 39, 3, 77);
+const webBackgroundColor = Color.fromARGB(255, 39, 3, 77);
 const blueColor = Color.fromARGB(255, 72, 0, 255);
 const primaryColor = Color.fromARGB(255, 0, 0, 0);
 const secondaryColor = Color.fromARGB(255, 255, 255, 255);
